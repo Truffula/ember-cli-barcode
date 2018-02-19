9ff6013695350b49af6170906ad22f2fcaf3bade
Gregg Martell <gmartell@pobox.com>
2018-02-18 23:48:24 -0500

    beheaded bower, now npm

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 /* eslint-env node */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
   name: 'ember-cli-barcode',
 
   included(app) {
     this._super.included.apply(this, arguments);
-    app.import(app.bowerDirectory + '/JsBarcode/dist/JsBarcode.all.js');
+    app.import('vendor/jsbarcode.all.js');
+  },
+
+  treeForVendor(vendorTree) {    
+    var jsbarcodeTree = new Funnel(path.dirname(require.resolve('jsbarcode/dist/jsbarcode.all.js')), {
+      files: ['jsbarcode.all.js'],
+    });
+
+    return new MergeTrees([vendorTree, jsbarcodeTree]);
   },
 
   isDevelopingAddon() {
