6517559ea4c78e0a1728e4e9b662a9af8602f28e
Gregg Martell <gmartell@pobox.com>
2018-02-19 01:09:54 -0500

    adding depencies

--- config/ember-try.js
+++ config/ember-try.js
@@ -36,6 +36,11 @@ module.exports = {
       npm: {
         devDependencies: {
           'ember-source': '~2.16.0'
+        },
+        dependencies: {
+          'jsbarcode': '~3.8.0',
+          'broccoli-funnel': '^2.0.1',
+          'broccoli-merge-trees': '^2.0.0'
         }
       }
     },
